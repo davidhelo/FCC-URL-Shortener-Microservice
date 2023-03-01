924b1e2f0a93d7052a54c67c6b4746946c43446b
davidhelo <dohernandez93@gmail.com>
2023-03-01 00:07:18 +0000

    feat: API POST and GET shorturl functionallity added

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+let shorturlIndex = 0;
+let shorturlIndexes = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +23,30 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.use('/api/shorturl', bodyParser.urlencoded({extended: false}));
+
+function URL_FORM_HANDLER(req, res) {
+  dns.lookup(req.body.url.replace(/^http[s]?:\/\/www./ig, ''), (error, addresses, family) => {
+    if (error != null) {
+      res.json({ error: 'invalid url' });
+    } else {
+      shorturlIndex += 1;
+      shorturlIndexes.push({ 
+        "original": req.body.url,
+        "short_url": shorturlIndex
+      });
+      res.json(shorturlIndexes[shorturlIndexes.length - 1]);
+    }
+  });
+}
+
+app.post('/api/shorturl', URL_FORM_HANDLER);
+
+app.get('/api/shorturl/:shorturl', function (req, res) {
+  let shorturl_Requested = parseInt(req.params.shorturl);
+  res.redirect(shorturlIndexes[shorturl_Requested - 1].original);
 });
 
 app.listen(port, function() {
